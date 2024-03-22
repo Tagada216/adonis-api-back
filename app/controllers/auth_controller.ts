afaba3c0fa128eb47355258eac32697cf9e3a5b8
Tagada216 <nicolasdelcourt10@gmail.com>
2024-03-22 11:59:07 +0100

    Add basic login Auth + Get ME

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -1,5 +1,5 @@
 import type { HttpContext } from '@adonisjs/core/http'
-import { registerValidator } from '#validators/auth'
+import { registerValidator, loginValidator } from '#validators/auth'
 import User from '#models/user'
 
 export default class AuthController {
@@ -7,5 +7,18 @@ export default class AuthController {
     const payload = await request.validateUsing(registerValidator)
     const user = await User.create(payload)
     return response.status(201).json(user)
+  }
+
+  async login({ request, response }: HttpContext) {
+    const { email, password } = await request.validateUsing(loginValidator)
+
+    const user = await User.verifyCredentials(email, password)
+
+    const token = await User.accessTokens.create(user)
+
+    return response.ok({
+      token: token,
+      ...user.serialize(),
+    })
   }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -8,6 +8,7 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
 const AuthController = () => import('#controllers/auth_controller')
 
@@ -18,8 +19,21 @@ router
         router
           .group(() => {
             router.post('register', [AuthController, 'register'])
+            router.post('login', [AuthController, 'login'])
           })
-          .prefix('auth')
+          .prefix('auth') // Auth routes ---
+        router
+          .get('me', async ({ auth, response }) => {
+            try {
+              const user = auth.getUserOrFail()
+              return response.ok(user)
+            } catch (error) {
+              return response.unauthorized({
+                message: 'You are not authorized to access this resource',
+              })
+            }
+          })
+          .use(middleware.auth()) // Protected routes ---
       })
       .prefix('v1')
   })
