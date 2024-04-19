6b59d6efe135f871f13740f0ed035e0bf364acc4
Tagada216 <nicolasdelcourt10@gmail.com>
2024-04-19 12:21:40 +0200

    Add birthdate + age automaticaly

--- database/migrations/1713262742466_create_profiles_table.ts
+++ database/migrations/1713262742466_create_profiles_table.ts
@@ -8,7 +8,7 @@ export default class extends BaseSchema {
       table.increments('id')
       table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
       table.string('first_name')
-      table.integer('age')
+      table.date('dob').nullable()
       table.string('gender')
 
       table.timestamps()
